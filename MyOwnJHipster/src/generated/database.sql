659d5047f89b6c6561b1c4eb446527833679af0b
rvcuban <jrodrigufxj@alumnos.unex.es>
2023-05-11 18:16:10 +0200

    Proyecto java y aceelon

---
+++ MyOwnJHipster/src/generated/database.sql
@@ -0,0 +1,19 @@
+
+-- Crear la base de datos
+CREATE DATABASE IF NOT EXISTS mi_base_de_datos;
+USE mi_base_de_datos;
+
+-- Crear las tablas
+CREATE TABLE IF NOT EXISTS  (
+  color VARCHAR(255)  PRIMARY KEY  
+   VARCHAR(255)
+);
+CREATE TABLE IF NOT EXISTS  (
+  pulgadas VARCHAR(255)  PRIMARY KEY  
+   VARCHAR(255)
+);
+CREATE TABLE IF NOT EXISTS  (
+  nucleos VARCHAR(255)  PRIMARY KEY  
+   VARCHAR(255)
+);
+
